eea90c82ea91e1c5f21a940b433b22d8088ee1e2
Igor Fernandes <igor_cesari@hotmail.com>
2021-09-29 15:00:26 -0300

    CSS MaterialUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+          <CssBaseline />
+          <Component {...pageProps} />
+  </>
+  );
 }
+
 export default MyApp
